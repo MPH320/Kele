4943d9bf94d321e584a6a4da9561c4dae192584d
Michelangelo Pereira <michelangelo.ph@gmail.com>
2017-05-15 23:17:54 +0000

    Convert user data to a Ruby hash

--- Kele/lib/kele.rb
+++ Kele/lib/kele.rb
@@ -9,6 +9,16 @@ require "httparty"
      raise "Invalid email or password" if response.code == 404
      @auth_token = response["auth_token"]
    end
+   
+   def get_me
+     response = self.class.get(base_api_endpoint("users/me"), headers: { "authorization" => @auth_token })
+     @user_data = JSON.parse(response.body)
+     @user_data.keys.each do |key|
+       self.class.send(:define_method, key.to_sym) do
+         @user_data[key]
+       end
+     end
+   end
  
  private
  
